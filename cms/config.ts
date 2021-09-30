3c273e2adc9642760892b821f782f3dca236da30
AJ Clemente <areljann.c@gmail.com>
2021-09-29 22:10:17 -0400

    Configure CMS configs

--- cms/config.ts
+++ cms/config.ts
@@ -1,4 +1,10 @@
-const config = {
+// EXTERNAL IMPORTS
+import { CmsConfig } from 'netlify-cms-core';
+
+const config: CmsConfig = {
+  // @ts-ignore
+  cms_manual_init: true,
+  local_backend: true,
   backend: {
     name: 'github',
     repo: 'areljannC/onlydrei.art',
@@ -10,6 +16,7 @@ const config = {
     {
       label: 'Pages',
       name: 'pages',
+      editor: { preview: false },
       files: [
         {
           label: 'Home',
@@ -17,13 +24,139 @@ const config = {
           file: 'content/pages/home.md',
           fields: [
             {
-              label: 'Title',
-              name: 'title',
+              label: 'Featured Pieces',
+              name: 'featuredPieces',
+              widget: 'list',
+              max: 6,
+              min: 1,
+              fields: [
+                {
+                  label: 'Cover Image',
+                  name: 'coverImage',
+                  widget: 'image',
+                  // @ts-ignore
+                  choose_url: false,
+                  required: true
+                },
+                {
+                  label: 'Name',
+                  name: 'name',
+                  widget: 'string',
+                  required: true
+                },
+                {
+                  label: 'Description',
+                  name: 'description',
+                  widget: 'string',
+                  required: false
+                },
+                {
+                  label: 'Slug (Page Route)',
+                  name: 'slug',
+                  widget: 'string',
+                  default: '/art/',
+                  required: true
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: 'About',
+          name: 'about',
+          file: 'content/pages/about.md',
+          fields: [
+            {
+              label: 'Headline',
+              name: 'headline',
+              widget: 'string',
+              required: true
+            },
+            {
+              label: 'Summary',
+              name: 'summary',
+              widget: 'markdown',
+              required: true
+            }
+          ]
+        }
+      ]
+    },
+    {
+      label: 'Art',
+      name: 'art',
+      editor: { preview: true },
+      create: true,
+      extension: 'md',
+      folder: 'content/art',
+      fields: [
+        {
+          label: 'Title',
+          name: 'title',
+          widget: 'string',
+          required: true
+        },
+        {
+          label: 'Description',
+          name: 'description',
+          widget: 'text',
+          required: false
+        },
+        {
+          label: 'Image',
+          name: 'image',
+          widget: 'image',
+          // @ts-ignore
+          choose_url: false,
+          required: true,
+        },
+        {
+          label: 'Date',
+          name: 'date',
+          widget: 'date',
+          required: true,
+          // @ts-ignore
+          format: 'DD MM YYYY',
+        },
+        {
+          label: 'Tags',
+          name: 'tags',
+          widget: 'list',
+          allow_add: true,
+          field: {
+            label: 'Tag',
+            name: 'tag',
+            widget: 'string',
+            required: true,
+          }
+        },
+        {
+          label: 'Slug (Page Route)',
+          name: 'slug',
+          widget: 'string',
+          default: '/art/',
+          required: true
+        }
+      ]
+    },
+    {
+      label: 'Settings',
+      name: 'settings',
+      editor: { preview: false },
+      files: [
+        {
+          label: 'Socials',
+          name: 'socials',
+          file: 'content/settings/socials.md',
+          fields: [
+            {
+              label: 'Instagram',
+              name: 'instagramLink',
               widget: 'string'
             },
             {
-              label: 'Description',
-              name: 'description',
+              label: 'TikTok',
+              name: 'tiktokLink',
               widget: 'string'
             }
           ]
